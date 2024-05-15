02f7e6820f843c16765b1b61ba9619d00bc1c655
rezafaraji <faraji.reza@outlook.com>
2024-05-15 15:14:36 -0400

    Setting up RunningTracker.kt

---
+++ run/domain/src/main/java/reza/droid/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,16 @@
+package reza.droid.run.domain
+
+import reza.droid.core.domain.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timestampsPerLine ->
+                timestampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+                }.sum().roundToInt()
+            }
+    }
+}
